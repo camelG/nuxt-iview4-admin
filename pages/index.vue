42634b75f5bce6797fe5755f98c9236bd16976f7
Tani <tani@next-investment.co.jp>
2019-11-05 11:54:45 +0900

    :tada: init

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>iView4 粋だね</h1>
+    <Slider v-model="value" range />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: [20, 50]
+    };
+  }
+};
+</script>
---
+++ plugins/iview.js
@@ -0,0 +1,7 @@
+import Vue from "vue";
+import iview from "view-design";
+
+// import style
+import "view-design/dist/styles/iview.css";
+
+Vue.use(iview);
