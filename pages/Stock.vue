5191fea66970b05a054c9d09f502c0d48aa9cecc
Tani <camel.tn@gmail.com>
2019-11-24 12:17:35 +0900

    update user stock

---
+++ pages/Stock.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Row>
+      <Col>
+      </Col>
+    </Row>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/User.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <Table :columns="columns1" :data="data1"></Table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      columns1: [
+        {
+          type: "selection",
+          width: 60,
+          align: "center"
+        },
+        {
+          title: "Name",
+          key: "name",
+          render: (h, params) => {
+            return h("div", [
+              h("Avatar", {
+                style: "margin-right: 5px"
+              },params.row.name && params.row.name.slice(0, 1)),
+              h("strong", params.row.name)
+            ]);
+          }
+        },
+        {
+          title: "Age",
+          key: "age"
+        },
+        {
+          title: "Address",
+          key: "address"
+        }
+      ],
+      data1: [
+        {
+          name: "John Brown",
+          age: 18,
+          address: "New York No. 1 Lake Park",
+          date: "2016-10-03"
+        },
+        {
+          name: "Jim Green",
+          age: 24,
+          address: "London No. 1 Lake Park",
+          date: "2016-10-01"
+        },
+        {
+          name: "Joe Black",
+          age: 30,
+          address: "Sydney No. 1 Lake Park",
+          date: "2016-10-02"
+        },
+        {
+          name: "Jon Snow",
+          age: 26,
+          address: "Ottawa No. 2 Lake Park",
+          date: "2016-10-04"
+        }
+      ]
+    };
+  }
+};
+</script>
